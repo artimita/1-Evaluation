7895a435ede32635bc18f214e99477323326fdd0
artima <mitraizadi65@gmail.com>
2023-07-28 09:43:08 +0200

    Premier commit

---
+++ js/file.js
@@ -0,0 +1,57 @@
+const myCarouselElement = document.querySelector("#myCarousel");
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 5000,
+  touch: false,
+});
+
+
+let pageNum = 1;
+let isLoading = false;
+const container = document.querySelector(".container");
+const loadingContainer = document.querySelector(".loading-container");
+const resultContainer = document.querySelector(".result-container");
+
+function fetchData() {
+  if (isLoading) return;
+  isLoading = true;
+  loadingContainer.innerHTML = "Loading ...";
+  fetch(`https://api.../issues?page=${pageNum}`)
+    .then((res) => {
+      res
+        .json()
+        .then((finalRes) => {
+          finalRes.forEach((item) => {
+            const resultDiv = document.createElement("div");
+            resultDiv.textContent = item.title;
+            resultDiv.classList.add("search-data");
+            resultContainer.appendChild(resultDiv);
+          });
+          isLoading = false;
+          loadingContainer.innerHTML = "";
+        })
+        .catch((e) => {
+          isLoading = false;
+          loadingContainer.innerHTML = "";
+        });
+    })
+    .catch((err) => {
+      isLoading = false;
+      loadingContainer.innerHTML = "";
+    });
+}
+
+container.onscroll = () => {
+  if (isLoading) return;
+
+  if (
+    Math.ceil(container.clientHeight + container.scrollTop) >=
+    container.scrollHeight
+  ) {
+    pageNum++;
+    fetchData();
+  }
+};
+
+fetchData();
+
